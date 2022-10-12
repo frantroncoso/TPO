0220a5dd39597b8951374c532ecd85a8f518bc03
francodealesandro <Francodealesandro@gmail.com>
2022-10-12 16:08:14 -0300

    ui/api

---
+++ api/src/main/java/com/CSEMDEINTPRO/tpo/TpoApiApplication.java
@@ -0,0 +1,13 @@
+package com.CSEMDEINTPRO.tpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/CSEMDEINTPRO/tpo/TpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSEMDEINTPRO.tpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
