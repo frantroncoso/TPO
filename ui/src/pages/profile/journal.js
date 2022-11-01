63dfafd4e74da3795ac278ac6ea6df4bfbb0cddd
matumolise <matumolise@gmail.com>
2022-11-01 17:13:38 -0300

    update journal

--- ui/src/pages/profile/journal.js
+++ ui/src/pages/profile/journal.js
@@ -1,10 +1,92 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+import { Divider } from "@mui/material/divider";
+import { Button } from "@mui/material";
 
 export default function Journal() {
+
+  const [resultsList, setresultsList] = useState([{
+    tratamiento: "Traumatología - Zona Hombros",
+    doctor: " Pablo Perez",
+    estado: "En Proceso",
+  },
+  {
+    tratamiento: "Plan de Alimentación",
+    doctor: " Juan Perez",
+    estado: "Finalizado",
+  },
+  {
+    tratamiento: "Kinesiología",
+    doctor: " Alberto Perez",
+    estado: "En Proceso",
+  },]);
+
+  
+
+
+  const handleCardEvent = () => {
+    console.log("mostrar pagina tratamiento");
+  };
+
   return (
-    <Typography variant="h5" color="inherit" paragraph>
-      Seguimiento
-    </Typography>
+      <Grid
+          item
+          width="100%"
+          height="100%"
+          sx={{
+            backgroundColor: "#00aae4",
+          }}
+        >
+          <Grid
+            container
+            component="main"
+            sx={{ height: "100%", width: "100%" }}
+          >
+            {resultsList.map((singleResult, index) => (
+              <Grid item>
+                <Card
+                  key={index}
+                  sx={{
+                    width: 1550,
+                    backgroundColor: "#F5DEB3",
+                    border: " 2px solid #ffff",
+                  }}
+                  
+                  onClick={handleCardEvent}
+                >
+              
+                  <CardContent>
+                    <Stack direction="row" spacing={5} justifyContent="space-between">
+                      <Typography variant="body2" color="text.secondary">
+                        {singleResult.tratamiento}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {"Doctor:    "}
+                        {singleResult.doctor}
+                      </Typography>
+
+                      <Button>
+                        Comentarios
+                        </Button>     
+                        <Button>
+                        Seguimientos
+                        </Button>                   
+                      <Typography variant="body2" color="text.secondary">
+                        {"Estado:     "}
+                        {singleResult.estado}
+                      </Typography>
+                    </Stack>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
   );
 }
