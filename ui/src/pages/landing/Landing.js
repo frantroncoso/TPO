61ef15bc6346b66bbecdde47ee3d69d8e535d63f
mateori0s <mateorios@live.com>
2022-11-15 12:54:37 -0300

    pruebas

--- ui/src/pages/landing/Landing.js
+++ ui/src/pages/landing/Landing.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Box, Typography, ImageList, ImageListItem, ButtonGroup,Button, Container, Grid, Stack, Item, Paper  } from "@mui/material";
+import { Box, Typography, ImageList, ImageListItem, ButtonGroup,
+  Button, Container, Grid, Stack, Item, Paper, Divider  } from "@mui/material";
 import { styled } from '@mui/material/styles';
 export default function Landing() {
   const images = [
@@ -16,10 +17,41 @@ export default function Landing() {
       img: "img/landing3.jpg",
     },
   ];
-  const buttons = [
-    <Button key="one">Si</Button>,
-    <Button key="two">No</Button>,
-  ];
+
+  const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 24,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: '#000',
+    borderColor: '#000',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#fff',
+      borderColor: '#000',
+      boxShadow: 'none',
+      color: 'black'
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#fff',
+      borderColor: '#fff',
+    },
+  });
+
 
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -31,8 +63,9 @@ export default function Landing() {
 
   return (
     <div>
-      <Container maxWidth="sm">
 
+      <Container 
+      fixed>
       {/* <Button variant="outlined"  
       size="large"
         onClick={() => {
@@ -40,37 +73,47 @@ export default function Landing() {
         }}
       >Contanos que necesitás y te ayudamos a buscar al profesional adecuado</Button> */}
 
-        <Box sx={{ width: '100%', maxWidth: 500 }}>
-          <Typography variant="h3" gutterBottom>
-            Ya te atendiste con un especialista?
-          </Typography>
-        </Box>
-
-        <Stack spacing={2}>
-          <Item> 
-          <Button variant="outlined"  
-      size="large"
-        onClick={() => {
-          alert('te lleva a opciones');
-        }}
-      >Si</Button>
-          </Item>
-          <Item>
-          <Button variant="outlined"  
-      size="large"
-        onClick={() => {
-          alert('te lleva a opciones');
-        }}
-      >No</Button>
-            </Item>
-        </Stack>
+        <Typography variant="h2" gutterBottom>
+          Ya te atendiste con un especialista?
+        </Typography>
 
     {/* 
       <ButtonGroup size="large" aria-label="large button group" >
         {buttons}
       </ButtonGroup> */}
+
       </Container>
-      
+      <Stack
+        direction="column-reverse"
+        justifyContent="flex-end"
+        alignItems="stretch"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={1}
+      >
+        {/* <Item>
+          <Button variant="text"
+            size="large"
+            onClick={() => {
+              alert('te lleva a opciones');
+            }}  
+          >No</Button>
+        </Item> */}
+
+        {/* <Button variant="contained"
+          size="large"
+          onClick={() => {
+            alert('te lleva a opciones');
+          }}
+        >Si</Button> */}
+        <BootstrapButton variant="contained" disableRipple>
+          No
+        </BootstrapButton>
+        <BootstrapButton variant="contained" disableRipple>
+        Si
+      </BootstrapButton>
+      </Stack>
+
+
     </div>
 
   );
